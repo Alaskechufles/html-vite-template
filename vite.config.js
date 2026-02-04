148c42d3b3505415d727694495a8285c88ec549c
Diego <diegohuarsayacruz@gmail.com>
2026-02-04 14:57:14 -0500

    vite config configurado

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build:{
+    rollupOptions:{
+      input:{
+        main: resolve(__dirname,'index.html'),
+        blog: resolve(__dirname,'pages/blog-card.html'),
+        qr: resolve(__dirname, 'pages/qr-card.html')
+      }
+    }
+  }
 })
